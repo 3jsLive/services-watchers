d262ede4c8e4f93b24d5d385721485caaf5b6287
3botjs <3botjs@users.noreply.github.com>
2019-11-03 12:21:02 +0100

    Split Runner stuff off into src/cli.js

--- src/BaseWatcher.js
+++ src/BaseWatcher.js
@@ -1,14 +1,11 @@
 const Promise = require( 'bluebird' );
 const request = Promise.promisify( require( 'request' ) );
-const signale = require( "signale" );
+const signale = require( 'signale' );
 const flatCache = require( 'flat-cache' );
 const execAsync = require( 'execasync' );
 
 
 signale.config( { displayTimestamp: true } );
-
-const statusLogger = signale.scope( 'BaseWatcher' );
-
 
 
 const config = {
@@ -38,34 +35,7 @@ const shellOptions = {
 };
 
 
-let boredomDuration;
-let boredomTimer;
-
 class BaseWatcher {
-
-	// there's got to be a better way for this
-	static get boredomTimer() {
-
-		return boredomTimer;
-
-	}
-	static set boredomTimer( value ) {
-
-		boredomTimer = value;
-
-	}
-
-	static get boredomDuration() {
-
-		return boredomDuration;
-
-	}
-	static set boredomDuration( value ) {
-
-		boredomDuration = value;
-
-	}
-
 
 	constructor( name, url ) {
 
@@ -112,7 +82,7 @@ class BaseWatcher {
 		logger.debug( 'etag', etag );
 
 		if ( ! callback )
-			callback = BaseWatcher.statusCallback;
+			callback = logger.debug;
 
 		return async () => {
 
@@ -213,46 +183,6 @@ class BaseWatcher {
 	}
 
 
-	static boredomFunc() {
-
-		statusLogger.pending( `Nothing happened in ${BaseWatcher.boredomDuration} seconds` );
-
-	}
-
-
-	static statusCallback( scope ) {
-
-		const logger = signale.scope( scope );
-
-		return function ( status ) {
-
-			clearInterval( BaseWatcher.boredomTimer );
-
-			if ( typeof status === 'string' ) {
-
-				logger.debug( status );
-
-			} else if ( status instanceof Error ) {
-
-				logger.info( 'Resuming...' );
-
-			} else if ( typeof status === 'number' ) {
-
-				logger.success( 'Updates:', status );
-
-			} else {
-
-				logger.fatal( status );
-
-			}
-
-			BaseWatcher.boredomTimer = setInterval( BaseWatcher.boredomFunc, BaseWatcher.boredomDuration * 1000 );
-
-		};
-
-	}
-
-
 	static exec( command, additionalOptions ) {
 
 		const options = { ...shellOptions, additionalOptions };
@@ -261,59 +191,6 @@ class BaseWatcher {
 
 	}
 
-
-	static pollAsync( func, delay = 0, timeout = 30000 ) {
-
-		const fullfilled = () => {
-
-			return Promise
-				.delay( delay )
-				.then( () => BaseWatcher.pollAsync( func, delay ) )
-				.timeout( timeout )
-				.catch( Promise.TimeoutError, () => console.log( 'pollAsync timed out' ) )
-				.catch( err => console.error( 'Something went wrong during polling:', err ) );
-
-		};
-
-		func()
-			.then( fullfilled )
-			.catch( fullfilled );
-
-	}
-
 }
 
 module.exports = BaseWatcher;
-
-BaseWatcher.boredomDuration = 90;
-BaseWatcher.boredomTimer = setInterval( BaseWatcher.boredomFunc, BaseWatcher.boredomDuration * 1000 );
-
-// pullrequests watcher is the small-scale watcher behind 3jslive
-// keeping the PR database up to date and that's about it
-const PullrequestsWatcher = require( './watchers/pullrequests' );
-const pullrequestsAPIStuffWatcher = new PullrequestsWatcher();
-BaseWatcher.pollAsync( pullrequestsAPIStuffWatcher.polling(), 5000 );
-
-// keeps our mirror of github meta stuff (issues, milestones, ...)
-// current, mostly for statistics in 3ci
-const EventsWatcher = require( './watchers/events' );
-const eventsLoggerWatcher = new EventsWatcher();
-BaseWatcher.pollAsync( eventsLoggerWatcher.polling(), 5000 );
-
-// only processes milestone/demilestone events because /watchers/events doesn't get that
-const MilestoningWatcher = require( './watchers/issues-events' );
-const milestoningLoggerWatcher = new MilestoningWatcher();
-BaseWatcher.pollAsync( milestoningLoggerWatcher.polling(), 5000 );
-
-// keep our fork's branches in sync with upstreams and split up
-// any multi-commit push into individual pushes so CI gets triggered
-// on all of them
-const BranchWatcher = require( './watchers/branch' );
-const branchMirrorWatcher = new BranchWatcher();
-BaseWatcher.pollAsync( branchMirrorWatcher.polling(), 5000 );
-
-// mirror PRs from mrdoob/three.js to our repo while also
-// splitting them up into one-commit-per-push
-const PrMirrorWatcher = require( './watchers/prMirror' );
-const pullrequestsMirrorWatcher = new PrMirrorWatcher();
-BaseWatcher.pollAsync( pullrequestsMirrorWatcher.polling(), 5000 );
---
+++ src/cli.js
@@ -0,0 +1,99 @@
+const Promise = require( 'bluebird' );
+const signale = require( "signale" );
+
+
+signale.config( { displayTimestamp: true } );
+const statusLogger = signale.scope( 'Runner' );
+
+
+const boredomDuration = 90;
+const boredomTimer = setInterval( boredomFunc, boredomDuration * 1000 );
+
+function boredomFunc() {
+
+	statusLogger.pending( `Nothing happened in ${boredomDuration} seconds` );
+
+}
+
+
+function statusCallback( scope ) {
+
+	const logger = signale.scope( scope );
+
+	return function ( status ) {
+
+		clearInterval( boredomTimer );
+
+		if ( typeof status === 'string' ) {
+
+			logger.debug( status );
+
+		} else if ( status instanceof Error ) {
+
+			logger.info( 'Resuming...' );
+
+		} else if ( typeof status === 'number' ) {
+
+			logger.success( 'Updates:', status );
+
+		} else {
+
+			logger.fatal( status );
+
+		}
+
+		boredomTimer = setInterval( boredomFunc, boredomDuration * 1000 );
+
+	};
+
+}
+
+
+function pollAsync( func, delay = 0, timeout = 30000 ) {
+
+	const fullfilled = () => {
+
+		return Promise
+			.delay( delay )
+			.then( () => pollAsync( func, delay ) )
+			.timeout( timeout )
+			.catch( Promise.TimeoutError, () => console.log( 'pollAsync timed out' ) )
+			.catch( err => console.error( 'Something went wrong during polling:', err ) );
+
+	};
+
+	func()
+		.then( fullfilled )
+		.catch( fullfilled );
+
+}
+
+
+// pullrequests watcher is the small-scale watcher behind 3jslive
+// keeping the PR database up to date and that's about it
+const PullrequestsWatcher = require( './watchers/pullrequests' );
+const pullrequestsAPIStuffWatcher = new PullrequestsWatcher();
+pollAsync( pullrequestsAPIStuffWatcher.polling( statusCallback( pullrequestsAPIStuffWatcher.name ) ), 5000 );
+
+// keeps our mirror of github meta stuff (issues, milestones, ...)
+// current, mostly for statistics in 3ci
+const EventsWatcher = require( './watchers/events' );
+const eventsLoggerWatcher = new EventsWatcher();
+pollAsync( eventsLoggerWatcher.polling( statusCallback( eventsLoggerWatcher.name ) ), 5000 );
+
+// only processes milestone/demilestone events because /watchers/events doesn't get that
+const MilestoningWatcher = require( './watchers/issues-events' );
+const milestoningLoggerWatcher = new MilestoningWatcher();
+pollAsync( milestoningLoggerWatcher.polling( statusCallback( milestoningLoggerWatcher.name ) ), 5000 );
+
+// keep our fork's branches in sync with upstreams and split up any
+// multi-commit push into individual pushes so CI gets triggered on all of them
+const BranchWatcher = require( './watchers/branch' );
+const branchMirrorWatcher = new BranchWatcher();
+pollAsync( branchMirrorWatcher.polling( statusCallback( branchMirrorWatcher.name ) ), 5000 );
+
+// mirror PRs from mrdoob/three.js to our repo while also
+// splitting them up into one-commit-per-push
+const PrMirrorWatcher = require( './watchers/prMirror' );
+const pullrequestsMirrorWatcher = new PrMirrorWatcher();
+pollAsync( pullrequestsMirrorWatcher.polling( statusCallback( pullrequestsMirrorWatcher.name ) ), 5000 );
