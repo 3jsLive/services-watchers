dfc3d9a7ef765563295f5a52f0015446b2cd1fd8
3botjs <3botjs@users.noreply.github.com>
2019-11-03 14:26:19 +0100

    reformatting

--- src/cli.js
+++ src/cli.js
@@ -69,31 +69,34 @@ function pollAsync( func, delay = 0, timeout = 30000 ) {
 }
 
 
+const PullrequestsWatcher = require( './watchers/pullrequests' );
+const EventsWatcher = require( './watchers/events' );
+const MilestoningWatcher = require( './watchers/issues-events' );
+const BranchWatcher = require( './watchers/branch' );
+const PrMirrorWatcher = require( './watchers/prMirror' );
+
+const pullrequestsAPIStuffWatcher = new PullrequestsWatcher();
+const eventsLoggerWatcher = new EventsWatcher();
+const milestoningLoggerWatcher = new MilestoningWatcher();
+const branchMirrorWatcher = new BranchWatcher();
+const pullrequestsMirrorWatcher = new PrMirrorWatcher();
+
+
 // pullrequests watcher is the small-scale watcher behind 3jslive
 // keeping the PR database up to date and that's about it
-const PullrequestsWatcher = require( './watchers/pullrequests' );
-const pullrequestsAPIStuffWatcher = new PullrequestsWatcher();
 pollAsync( pullrequestsAPIStuffWatcher.polling( statusCallback( pullrequestsAPIStuffWatcher.name ) ), 5000 );
 
 // keeps our mirror of github meta stuff (issues, milestones, ...)
 // current, mostly for statistics in 3ci
-const EventsWatcher = require( './watchers/events' );
-const eventsLoggerWatcher = new EventsWatcher();
 pollAsync( eventsLoggerWatcher.polling( statusCallback( eventsLoggerWatcher.name ) ), 5000 );
 
 // only processes milestone/demilestone events because /watchers/events doesn't get that
-const MilestoningWatcher = require( './watchers/issues-events' );
-const milestoningLoggerWatcher = new MilestoningWatcher();
 pollAsync( milestoningLoggerWatcher.polling( statusCallback( milestoningLoggerWatcher.name ) ), 5000 );
 
 // keep our fork's branches in sync with upstreams and split up any
 // multi-commit push into individual pushes so CI gets triggered on all of them
-const BranchWatcher = require( './watchers/branch' );
-const branchMirrorWatcher = new BranchWatcher();
 pollAsync( branchMirrorWatcher.polling( statusCallback( branchMirrorWatcher.name ) ), 5000 );
 
 // mirror PRs from mrdoob/three.js to our repo while also
 // splitting them up into one-commit-per-push
-const PrMirrorWatcher = require( './watchers/prMirror' );
-const pullrequestsMirrorWatcher = new PrMirrorWatcher();
 pollAsync( pullrequestsMirrorWatcher.polling( statusCallback( pullrequestsMirrorWatcher.name ) ), 5000 );
